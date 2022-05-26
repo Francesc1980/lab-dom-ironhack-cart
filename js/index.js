0576e8490b5b347d99ca57eb96824bd676ecc988
francesc duran <duranpaco@yahoo.com>
2022-05-26 19:25:45 +0200

    cambios ejercicio

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 // ITERATION 1
 
+const { product } = require("puppeteer");
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('quantity input').value;
+  // la suma total es igual al precio por cantidad
+  let sumTotal = price * quantity;
 }
+
+
+// accedo al subtotal para que me de la suma total
+let subtotalCell = document.querySelector(".subtotal");
+subtotalCell.textContent = sumTotal;
+
+// asignamos a la propiedad .textcontent del elemento del dom <span> que es decendiente</span>
+//precio por cantidad
+product.queryselector("subtotal span").textContent = price * quantity;
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
@@ -14,7 +30,10 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let products = document.querySelectorAll(".product");
+  for (let i = 0; i < prodducts .lenght; i ++){
+    upadateSubtotal(products[i]);
+  }
 
   // ITERATION 3
   //... your code goes here
